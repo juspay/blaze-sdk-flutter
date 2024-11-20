293175953633e56ee518a5e605ea7d70daa67cb2
Sahil Sinha <sahilsinha.dar@gmail.com>
2024-11-20 11:06:36 +0530

    BZ-36564: feat: add native interfaces for iOS
    - added blaze sdk ios dependency & it's interfaces
    - updated android sdk version to 0.0.18 for latest android sdk
    - updated example to simulate headless custom flow

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -45,8 +45,8 @@ class _MyAppState extends State<MyApp> {
   Map<String, dynamic> createInitiatePayload() {
     return {
       "merchantId": "d2cstorebeta",
-      "environment": "sandbox",
-      "shopUrl": "https://d2c-store-beta.myshopify.com"
+      "environment": "beta",
+      "shopUrl": "https://d2cstore-custom.vercel.app"
     };
   }
 
@@ -57,78 +57,11 @@ class _MyAppState extends State<MyApp> {
   Map<String, dynamic> createProcessPayload() {
     return {
       "action": "startCheckout",
-      "cart": {
-        "token": "c1-473243a885065ad7c911dc334255d73e",
-        "note": "",
-        "attributes": {
-          "bundleData": "-",
-          "discountName": "-",
-          "variant_ids": "-",
-          "target_variant_ids": "-",
-          "discount": "-"
-        },
-        "original_total_price": 100,
-        "total_price": 100,
-        "total_discount": 0,
-        "total_weight": 0.0,
-        "item_count": 1,
-        "items": [
-          {
-            "id": 45938457084219,
-            "properties": {},
-            "quantity": 1,
-            "variant_id": 45938457084219,
-            "key": "45938457084219:7df34184-c212-44dc-8d8e-8954b890646d",
-            "title": "Sony PS5 Digital Standalone",
-            "price": 100,
-            "original_price": 100,
-            "discounted_price": 100,
-            "line_price": 100,
-            "original_line_price": 100,
-            "total_discount": 0,
-            "discounts": [],
-            "sku": "",
-            "grams": 0,
-            "vendor": "Sony",
-            "taxable": true,
-            "product_id": 8561540628795,
-            "product_has_only_default_variant": true,
-            "gift_card": false,
-            "final_price": 100,
-            "final_line_price": 100,
-            "url":
-                "/products/sony-ps5-digital-standalone?variant=45938457084219",
-            "featured_image": {
-              "aspect_ratio": 1.0,
-              "alt": "Sony PS5 Digital Standalone",
-              "height": 679,
-              "url":
-                  "https://cdn.shopify.com/s/files/1/0684/1624/1979/files/51wPWj--fAL._SX679.jpg?v=1690641107",
-              "width": 679
-            },
-            "image":
-                "https://cdn.shopify.com/s/files/1/0684/1624/1979/files/51wPWj--fAL._SX679.jpg?v=1690641107",
-            "handle": "sony-ps5-digital-standalone",
-            "requires_shipping": true,
-            "product_type": "",
-            "product_title": "Sony PS5 Digital Standalone",
-            "product_description":
-                "Maximize your play sessions with near instant load times for installed PS5 games. The custom integration of the PS5 console's systems lets creators pull data from the SSD so quickly that they can design games in ways never before possible. Immerse yourself in worlds with a new level of realism as rays of light are individually simulated, creating true-to-life shadows and reflections in supported PS5 games. Play your favorite PS5 games on your stunning 4K TV. Enjoy smooth and fluid high frame rate gameplay at up to 120fps for compatible games, with support for 120Hz output on 4K displays. With an HDR TV, supported PS5 games display an unbelievably vibrant and lifelike range of colors. PS5 consoles support 8K Output, so you can play games on your 4320p resolution display. Immerse yourself in soundscapes where it feels as if the sound comes from every direction. Through your headphones or TV speakers your surroundings truly come alive with Tempest 3D AudioTech in supported games. Experience haptic feedback via the DualSense wireless controller in select PS5 titles and feel the effects and impact of your in-game actions through dynamic sensory feedback. Get to grips with immersive adaptive triggers, featuring dynamic resistance levels which simulate the physical impact of in-game activities in select PS5 games.",
-            "variant_title": null,
-            "variant_options": ["Default Title"],
-            "options_with_values": [
-              {"name": "Title", "value": "Default Title"}
-            ],
-            "line_level_discount_allocations": [],
-            "line_level_total_discount": 0,
-            "has_components": false
-          }
-        ],
-        "requires_shipping": true,
-        "currency": "INR",
-        "items_subtotal_price": 100,
-        "cart_level_discount_applications": []
-      }
+      "cart":
+          "{\"id\":\"7feaa429-308d-49b7-8461-46bfa4b37ff7\",\"items\":[{\"id\":\"3aecf419-6823-4581-a093-0c90c5b5e1fc\",\"title\":\"Apple iPhone 7 Plus\",\"variantTitle\":\"256 GB / GOLD\",\"image\":\"https://dfelk5npz6ka0.cloudfront.net/products/40767940985052.jpg\",\"quantity\":1,\"initialPrice\":100,\"finalPrice\":100,\"discount\":0}],\"initialPrice\":100,\"totalPrice\":100,\"totalDiscount\":0,\"itemCount\":1,\"currency\":\"INR\"}",
+      "signature":
+          "T4BRKwHKWfkWRLgF5ecss2+2tyID4zKVUqkfEgdFS73lyAokBq92VJRz4g+xzCcUE84ZNl7oQ9t26i8zGQLgh/B/6vNliM9u7VMX0soYDC9pEWd4TsWeetlYMzl/UIzitYan5q9aQ2UfS7HENHQGvGfOjsa75gP3SVwpufK8Sb1VFRFsnJKsXzgWq+y9iWLieJe596poEzUP2Wkt17mSGwH9rWukTy5S1ddZoZXUxDQiMtfXFj9NOnPDUA8psmERifivaD8IvhMEYwlEk+u6MrxF7IhasBs8dYGEZW+YDeaRz6mzK37sarYWgCUJgEYTJ97rmcRedgvnj+28YES/pw==",
+      "keyId": "90701"
     };
   }
 
@@ -149,7 +82,7 @@ class _MyAppState extends State<MyApp> {
                       createSDKPayload(createInitiatePayload()),
                       handleCallbackEvent);
                 },
-                child: const Text('Initiate'),
+                child: const Text('Initiate SDK'),
               ),
               ElevatedButton(
                 onPressed: () {
@@ -157,6 +90,12 @@ class _MyAppState extends State<MyApp> {
                       .process(createSDKPayload(createProcessPayload()));
                 },
                 child: const Text('Process'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  _blazeSdkFlutterPlugin.terminate();
+                },
+                child: const Text('Terminate'),
               )
             ],
           ),
