32107794848564cc597ddb5fb802e911c216f007
Sahil Sinha <sahilsinha.dar@gmail.com>
2025-09-19 12:23:11 +0530

    BZ-43702: chore: upgrade ios & android SDKs
    - upgraded iOS Blaze SDK to 0.5.0
    - upgraded Android Blaze SDK to 0.0.19-alpha
    - example updates:
        - upgraded Android Gradle plugin to 8.11.1
        - upgraded Gradle wrapper to 8.13
        - upgraded Android NDK to 27.0.12077973
        - added URL scheme queries in Info.plist
        - improved cart payload structure in main.dart

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -44,9 +44,9 @@ class _MyAppState extends State<MyApp> {
 
   Map<String, dynamic> createInitiatePayload() {
     return {
-      "merchantId": "d2cstorebeta",
-      "environment": "beta",
-      "shopUrl": "https://d2cstore-custom.vercel.app"
+      "merchantId": "d2cstore",
+      "environment": "release",
+      "shopUrl": "https://breeze-it-store.myshopify.com"
     };
   }
 
@@ -57,11 +57,73 @@ class _MyAppState extends State<MyApp> {
   Map<String, dynamic> createProcessPayload() {
     return {
       "action": "startCheckout",
-      "cart":
-          "{\"id\":\"7feaa429-308d-49b7-8461-46bfa4b37ff7\",\"items\":[{\"id\":\"3aecf419-6823-4581-a093-0c90c5b5e1fc\",\"title\":\"Apple iPhone 7 Plus\",\"variantTitle\":\"256 GB / GOLD\",\"image\":\"https://dfelk5npz6ka0.cloudfront.net/products/40767940985052.jpg\",\"quantity\":1,\"initialPrice\":100,\"finalPrice\":100,\"discount\":0}],\"initialPrice\":100,\"totalPrice\":100,\"totalDiscount\":0,\"itemCount\":1,\"currency\":\"INR\"}",
-      "signature":
-          "T4BRKwHKWfkWRLgF5ecss2+2tyID4zKVUqkfEgdFS73lyAokBq92VJRz4g+xzCcUE84ZNl7oQ9t26i8zGQLgh/B/6vNliM9u7VMX0soYDC9pEWd4TsWeetlYMzl/UIzitYan5q9aQ2UfS7HENHQGvGfOjsa75gP3SVwpufK8Sb1VFRFsnJKsXzgWq+y9iWLieJe596poEzUP2Wkt17mSGwH9rWukTy5S1ddZoZXUxDQiMtfXFj9NOnPDUA8psmERifivaD8IvhMEYwlEk+u6MrxF7IhasBs8dYGEZW+YDeaRz6mzK37sarYWgCUJgEYTJ97rmcRedgvnj+28YES/pw==",
-      "keyId": "90701"
+      "cart": {
+        "token":
+            "hWN398h1MYYGTJK83e2rDznA?key=29cbc71d22b49a79cb8e25229de5813d",
+        "note": "",
+        "attributes": {},
+        "original_total_price": 1400,
+        "total_price": 1400,
+        "total_discount": 0,
+        "total_weight": 0.0,
+        "item_count": 2,
+        "items": [
+          {
+            "id": 44182614474984,
+            "properties": {},
+            "quantity": 2,
+            "variant_id": 44182614474984,
+            "key": "44182614474984:14a856f03fde63adbb7df3da56d64b10",
+            "title": "Blackout Shirt - black",
+            "price": 700,
+            "original_price": 700,
+            "presentment_price": 7.0,
+            "discounted_price": 700,
+            "line_price": 1400,
+            "original_line_price": 1400,
+            "total_discount": 0,
+            "discounts": [],
+            "sku": "1233",
+            "grams": 0,
+            "vendor": "Bombay Shirt Company",
+            "taxable": true,
+            "product_id": 8070198591720,
+            "product_has_only_default_variant": false,
+            "gift_card": false,
+            "final_price": 700,
+            "final_line_price": 1400,
+            "url": "\/products\/blackout-shirt?variant=44182614474984",
+            "featured_image": {
+              "aspect_ratio": 1.0,
+              "alt": "Blackout Shirt",
+              "height": 1200,
+              "url":
+                  "https:\/\/cdn.shopify.com\/s\/files\/1\/0647\/6526\/4104\/products\/bmb1.webp?v=1670259737",
+              "width": 1200
+            },
+            "image":
+                "https:\/\/cdn.shopify.com\/s\/files\/1\/0647\/6526\/4104\/products\/bmb1.webp?v=1670259737",
+            "handle": "blackout-shirt",
+            "requires_shipping": true,
+            "product_type": "",
+            "product_title": "Blackout Shirt",
+            "product_description": "Built for your adventures after dark.",
+            "variant_title": "black",
+            "variant_options": ["black"],
+            "options_with_values": [
+              {"name": "Color", "value": "black"}
+            ],
+            "line_level_discount_allocations": [],
+            "line_level_total_discount": 0,
+            "has_components": false
+          }
+        ],
+        "requires_shipping": true,
+        "currency": "INR",
+        "items_subtotal_price": 1400,
+        "cart_level_discount_applications": [],
+        "discount_codes": []
+      }
     };
   }
 
