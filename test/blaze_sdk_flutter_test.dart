27aa77803ff4bf2dbb50dbdf4146179d78d46e6f
Sahil Sinha <sahilsinha.dar@gmail.com>
2026-07-23 16:20:57 +0530

    ci: added workflow for publishing to pub.dev
    - added ci workflow for releasing to pub.dev
    - updated example and test

--- test/blaze_sdk_flutter_test.dart
+++ test/blaze_sdk_flutter_test.dart
@@ -6,21 +6,77 @@ import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 
 class MockBlazeSdkFlutterPlatform
     with MockPlatformInterfaceMixin
-    implements BlazeSdkFlutterPlatform {}
+    implements BlazeSdkFlutterPlatform {
+  final List<String> calls = [];
+  Map<String, dynamic>? lastPayload;
+
+  @override
+  Future<void> initiate(Map<String, dynamic> payload,
+      void Function(Map<String, dynamic>) callbackFn) async {
+    calls.add('initiate');
+    lastPayload = payload;
+  }
+
+  @override
+  Future<void> process(Map<String, dynamic> payload) async {
+    calls.add('process');
+    lastPayload = payload;
+  }
+
+  @override
+  Future<bool?> handleBackPress() async {
+    calls.add('handleBackPress');
+    return true;
+  }
+
+  @override
+  Future<void> terminate() async {
+    calls.add('terminate');
+  }
+}
 
 void main() {
   final BlazeSdkFlutterPlatform initialPlatform =
       BlazeSdkFlutterPlatform.instance;
 
   test('$MethodChannelBlazeSdkFlutter is the default instance', () {
-    expect(initialPlatform, isInstanceOf<MethodChannelBlazeSdkFlutter>());
+    expect(initialPlatform, isA<MethodChannelBlazeSdkFlutter>());
   });
 
-  test('getPlatformVersion', () async {
-    BlazeSdkFlutter blazeSdkFlutterPlugin = BlazeSdkFlutter();
-    MockBlazeSdkFlutterPlatform fakePlatform = MockBlazeSdkFlutterPlatform();
-    BlazeSdkFlutterPlatform.instance = fakePlatform;
+  group('BlazeSdkFlutter delegates to the platform instance', () {
+    late BlazeSdkFlutter plugin;
+    late MockBlazeSdkFlutterPlatform fakePlatform;
 
-    // TODO: Add test case implementation
+    setUp(() {
+      plugin = BlazeSdkFlutter();
+      fakePlatform = MockBlazeSdkFlutterPlatform();
+      BlazeSdkFlutterPlatform.instance = fakePlatform;
+    });
+
+    tearDown(() {
+      BlazeSdkFlutterPlatform.instance = initialPlatform;
+    });
+
+    test('initiate', () async {
+      await plugin.initiate({'clientId': 'demo'}, (_) {});
+      expect(fakePlatform.calls, ['initiate']);
+      expect(fakePlatform.lastPayload, {'clientId': 'demo'});
+    });
+
+    test('process', () async {
+      await plugin.process({'action': 'startCheckout'});
+      expect(fakePlatform.calls, ['process']);
+      expect(fakePlatform.lastPayload, {'action': 'startCheckout'});
+    });
+
+    test('handleBackPress', () async {
+      expect(await plugin.handleBackPress(), isTrue);
+      expect(fakePlatform.calls, ['handleBackPress']);
+    });
+
+    test('terminate', () async {
+      await plugin.terminate();
+      expect(fakePlatform.calls, ['terminate']);
+    });
   });
 }
